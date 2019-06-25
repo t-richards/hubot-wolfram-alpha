dd4e3f3f63676224564a79373eb1b644e8cbd28f
Jack Miner <3ch01c@gmail.com>
2019-06-24 23:42:20 -0600

    Create an attachment for each subpod

--- src/generate-attachments.js
+++ src/generate-attachments.js
@@ -5,16 +5,15 @@ module.exports = result => {
   let attachments = [];
 
   for (let pod of result.pods) {
-    let attachment = {
-      color: "#FF8700",
-      title: pod.title
-    };
     for (let subpod of pod.subpods) {
+      let attachment = {
+        color: "#FF8700",
+        title: pod.title
+      };
       attachment.fallback = subpod.plaintext;
       attachment.image_url = subpod.img.src;
+      attachments.push(attachment);
     }
-
-    attachments.push(attachment);
   }
 
   return attachments;
